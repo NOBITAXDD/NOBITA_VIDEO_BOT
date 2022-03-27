eccebfbbfedacdaa1400002fbfc02525616a4678
NOBITA_XD <101139721+NOBITAXDD@users.noreply.github.com>
2022-03-27 21:12:56 +0530

    Update video.py

--- plugins/video.py
+++ plugins/video.py
@@ -136,7 +136,7 @@ async def stream(client, m: Message):
         else:
             await msg.edit("🔄 `Starting Live Video Stream ...`")
             link = query
-            thumb = "https://telegra.ph/file/3e14128ad5c9ec47801bd.jpg"
+            thumb = "https://te.legra.ph/file/50a3a42a888d21ca5dddb.jpg"
 
         vid_call = VIDEO_CALL.get(chat_id)
         if vid_call:
@@ -188,7 +188,7 @@ async def stream(client, m: Message):
             lel = await client.download_media(lol['file_id'])
             thumb = lel
         else:
-            thumb = "https://telegra.ph/file/62e86d8aadde9a8cbf9c2.jpg"
+            thumb = "https://te.legra.ph/file/50a3a42a888d21ca5dddb.jpg"
 
         video = await client.download_media(media)
 
@@ -212,7 +212,7 @@ async def stream(client, m: Message):
             await msg.delete()
             await m.reply_photo(
                photo=thumb,
-               caption=f"▶️ **Started [Video Streaming](https://t.me/TheNavya) In {m.chat.title} !**",
+               caption=f"▶️ **Started [Video Streaming](https://t.me/AAPLI_YAARI) In {m.chat.title} !**",
                reply_markup=InlineKeyboardMarkup(
                [
                    [
